bd9125d6a3adb3c70df32c92bbc51fdd205361d6
sultan lök <12542002.sultan.lok@gmail.com>
2018-04-02 17:58:15 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace CsharpSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.arama = new System.Windows.Forms.GroupBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.hxfgh = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.numara = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.soyad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cinsiyet = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bolum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.yas = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.arama.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 201);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "KAYIT";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "düzenle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(74, 151);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(74, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Items.AddRange(new object[] {
+            "erkek",
+            "kız"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "YAŞ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "BOLUM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CİNSİYET";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SOYAD";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD";
+            // 
+            // arama
+            // 
+            this.arama.Controls.Add(this.textBox6);
+            this.arama.Controls.Add(this.label7);
+            this.arama.Controls.Add(this.hxfgh);
+            this.arama.Controls.Add(this.button4);
+            this.arama.Controls.Add(this.textBox5);
+            this.arama.Location = new System.Drawing.Point(354, 12);
+            this.arama.Name = "arama";
+            this.arama.Size = new System.Drawing.Size(226, 141);
+            this.arama.TabIndex = 1;
+            this.arama.TabStop = false;
+            this.arama.Text = "ARAMA";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(88, 63);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "ADI\r\n";
+            // 
+            // hxfgh
+            // 
+            this.hxfgh.AutoSize = true;
+            this.hxfgh.Location = new System.Drawing.Point(40, 70);
+            this.hxfgh.Name = "hxfgh";
+            this.hxfgh.Size = new System.Drawing.Size(47, 13);
+            this.hxfgh.TabIndex = 14;
+            this.hxfgh.Text = "SOYADI\r\n";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(113, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "ara";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(88, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.numara,
+            this.ad,
+            this.soyad,
+            this.cinsiyet,
+            this.bolum,
+            this.yas});
+            this.listView1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listView1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.listView1.Location = new System.Drawing.Point(2, 219);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(704, 445);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // numara
+            // 
+            this.numara.Text = "numara";
+            this.numara.Width = 104;
+            // 
+            // ad
+            // 
+            this.ad.Text = "ad";
+            this.ad.Width = 72;
+            // 
+            // soyad
+            // 
+            this.soyad.Text = "soyad";
+            this.soyad.Width = 77;
+            // 
+            // cinsiyet
+            // 
+            this.cinsiyet.Text = "cinsiyet";
+            this.cinsiyet.Width = 96;
+            // 
+            // bolum
+            // 
+            this.bolum.Text = "bolum";
+            this.bolum.Width = 102;
+            // 
+            // yas
+            // 
+            this.yas.Text = "yas";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(397, 173);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(155, 40);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "TÜM KAYITLAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(700, 692);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.arama);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.button5_Click);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.arama.ResumeLayout(false);
+            this.arama.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox arama;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColumnHeader numara;
+        private System.Windows.Forms.ColumnHeader ad;
+        private System.Windows.Forms.ColumnHeader soyad;
+        private System.Windows.Forms.ColumnHeader cinsiyet;
+        private System.Windows.Forms.ColumnHeader bolum;
+        private System.Windows.Forms.ColumnHeader yas;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label hxfgh;
+        public System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace CsharpSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        SqlConnection baglanti = new SqlConnection("Data Source=SAMSUNGPC;Initial Catalog=projeee;Integrated Security=true;");
+         
+        private void verilerigoster()
+        {
+            if (baglanti.State == ConnectionState.Closed) {
+            baglanti.Open();
+            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.Text = Convert.ToString("");
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+           listView1.Items.Clear();
+            try
+            {
+                SqlCommand kmt = new SqlCommand("select * from projemm", baglanti);
+                SqlDataReader rdr = kmt.ExecuteReader();
+                while (rdr.Read())
+                {
+                    ListViewItem li = new ListViewItem();
+                    li.Text = rdr["numara"].ToString();
+                    li.SubItems.Add(rdr["ad"].ToString());
+                    li.SubItems.Add(rdr["soyad"].ToString());
+                    li.SubItems.Add(rdr["cinsiyet"].ToString());
+                    li.SubItems.Add(rdr["bolum"].ToString());
+                    li.SubItems.Add(rdr["yas"].ToString());
+                    listView1.Items.Add(li);
+                }
+            }
+            catch { MessageBox.Show("hata!!"); }
+           baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {        //TÜM VERİLERİ GÖSTER    
+                verilerigoster();       
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {// KAYDET TUŞU
+            baglanti.Open(); textBox1.Focus();
+            if (textBox1.Text != "" && textBox2.Text != "" && comboBox1.Text !=Convert.ToString("") && textBox3.Text != "" && textBox4.Text != "")
+
+            {               
+                    SqlCommand komut = new SqlCommand("insert into projemm(ad,soyad,cinsiyet,bolum,yas)values('" + textBox1.Text.ToString() + "','" + textBox2.Text.ToString() + "','" + comboBox1.Text.ToString() + "','" + textBox3.Text.ToString() + "','" + textBox4.Text.ToString() + "')", baglanti);
+                    komut.ExecuteNonQuery();
+                    verilerigoster();
+                
+            }
+            else { MessageBox.Show("Lütfen tüm bilgilerinizi doldurunuz.."); }                
+         baglanti.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)  {   }
+        private void button2_Click(object sender, EventArgs e)
+        {//düzenle
+            baglanti.Open();
+            ata = Convert.ToInt32(listView1.SelectedItems[0].SubItems[0].Text);
+            SqlCommand komut = new SqlCommand("update projemm set ad='" + textBox1.Text.ToString() + "',soyad='" + textBox2.Text.ToString() + "',cinsiyet='" + comboBox1.Text.ToString() + "',bolum='" + textBox3.Text.ToString() + "',yas='" + textBox4.Text.ToString() + "' where numara=" + ata + "", baglanti);
+            komut.ExecuteNonQuery();
+            verilerigoster();
+            baglanti.Close();
+        }
+        int ata = 0;
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int ata = Convert.ToInt32(listView1.SelectedItems[0].SubItems[0].Text);
+                textBox1.Text = listView1.SelectedItems[0].SubItems[1].Text;
+                textBox2.Text = listView1.SelectedItems[0].SubItems[2].Text;
+                comboBox1.Text = listView1.SelectedItems[0].SubItems[3].Text;
+                textBox3.Text = listView1.SelectedItems[0].SubItems[4].Text;
+                textBox4.Text = listView1.SelectedItems[0].SubItems[5].Text;     
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {//sil tuşu
+            
+            baglanti.Open();
+            ata=Convert.ToInt32(listView1.SelectedItems[0].SubItems[0].Text);
+            SqlCommand komut = new SqlCommand("Delete from projemm where numara="+ata+"", baglanti);
+            komut.ExecuteNonQuery();
+            verilerigoster();
+            baglanti.Close();
+        }
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {// arama duşu
+            listView1.Items.Clear();
+            baglanti.Open();
+            SqlCommand kmt = new SqlCommand("select * from projemm where ad like'%" + textBox5.Text.ToString() + "%' and soyad like'%" + textBox6.Text.ToString() + "%'", baglanti);
+                SqlDataReader rdr = kmt.ExecuteReader();
+                while (rdr.Read())
+                {
+                    ListViewItem li = new ListViewItem();
+                    li.Text = rdr["numara"].ToString();
+                    li.SubItems.Add(rdr["ad"].ToString());
+                    li.SubItems.Add(rdr["soyad"].ToString());
+                    li.SubItems.Add(rdr["cinsiyet"].ToString());
+                    li.SubItems.Add(rdr["bolum"].ToString());
+                    li.SubItems.Add(rdr["yas"].ToString());
+                    listView1.Items.Add(li);
+                }
+            
+           baglanti.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        { //textBox4 e yazı girmesini engeller. (keypress özelliğinden )
+            e.Handled = Char.IsLetter(e.KeyChar);
+            
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsLetterOrDigit(e.KeyChar) || Char.IsSymbol(e.KeyChar) || Char.IsPunctuation(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || Char.IsNumber(e.KeyChar);
+
+        }
+
+       
+
+       
+    }
+}
